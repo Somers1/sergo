1129116d039e441d867570d0afabcbddd316b8ab
Al <al@clawd.bot>
2026-02-16 21:33:25 +1100

    Extract TransactSQLQuery into its own query/transactql.py

--- src/sergo/query/__init__.py
+++ src/sergo/query/__init__.py
@@ -1,3 +1,4 @@
-from sergo.query.base import BaseQuery, Query, TransactSQLQuery, _LazyQuery
+from sergo.query.base import BaseQuery, Query, _LazyQuery
+from sergo.query.transactql import TransactSQLQuery
 
 __all__ = ['BaseQuery', 'Query', 'TransactSQLQuery']
